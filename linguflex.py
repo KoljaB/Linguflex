e462aeaf98e02ef0c9ee27f3a422815995f90a9a
Kolja Beigel <kolja.beigel@web.de>
2023-06-28 23:27:17 +0200

    Add files via upload

--- linguflex.py
+++ linguflex.py
@@ -8,44 +8,42 @@ import sys
 sys.path.insert(0, 'modules')
 sys.path.insert(0, 'modules/basic')
 sys.path.insert(0, 'modules/full')
+sys.path.insert(0, 'core')
 
-from linguflex_log import (
-    log,
-    DEBUG_LEVEL_OFF,
-    DEBUG_LEVEL_MIN,
-    DEBUG_LEVEL_MID,
-    DEBUG_LEVEL_MAX,
-    debug_level,
-    set_external_method,
-    get_elapsed_time,
-)
-from linguflex_interfaces import (
-    Module_IF,
-    InputProviderModule_IF,
-    SpeechRecognitionModule_IF,
-    TextToSpeechModule_IF,
-    LanguageProcessingModule_IF,
-    JsonActionProviderModule_IF,
-)
-from linguflex_config import cfg
-from linguflex_message import LinguFlexMessage
-from linguflex_server import LinguFlexServer
+openai_function_call_module = importlib.import_module("linguflex_functions")
 
+from core import (
+    log, DEBUG_LEVEL_OFF, DEBUG_LEVEL_MIN, DEBUG_LEVEL_MID, DEBUG_LEVEL_MAX, DEBUG_LEVEL_ERR, debug_level, set_external_method, get_elapsed_time,
+    items,
+    BaseModule, InputModule, SpeechRecognitionModule, TextToSpeechModule, TextGeneratorModule, ActionModule,
+    Request,
+    LinguFlexServer)
+
+# log(DEBUG_LEVEL_MIN, 'Eins, zwei, drei, vier, fünf, sechs, sieben, acht, neun, zehn, elf, zwölf, dreizehn, vierzehn, fünfzehn, sechzehn, siebzehn, achtzehn, neunzehn, zwanzig, einundzwanzig, zweiundzwanzig, dreiundzwanzig, vierundzwanzig, fünfundzwanzig, sechsundzwanzig, siebenundzwanzig, achtundzwanzig, neunundzwanzig, dreißig, einunddreißig, zweiunddreißig, dreiunddreißig, vierunddreißig, fünfunddreißig, sechsunddreißig, siebenunddreißig, achtunddreißig, neununddreißig, vierzig, einundvierzig, zweiundvierzig, dreiundvierzig, vierundvierzig, fünfundvierzig, sechsundvierzig, siebenundvierzig, achtundvierzig, neunundvierzig, fünfzig, einundfünfzig, zweiundfünfzig, dreiundfünfzig, vierundfünfzig, fünfundfünfzig, sechsundfünfzig, siebenundfünfzig, achtundfünfzig, neunundfünfzig, sechzig, einundsechzig, zweiundsechzig, dreiundsechzig, vierundsechzig, fünfundsechzig, sechsundsechzig, siebenundsechzig, achtundsechzig, neunundsechzig, siebzig, einundsiebzig, zweiundsiebzig, dreiundsiebzig, vierundsiebzig, fünfundsiebzig, sechsundsiebzig, siebenundsiebzig, achtundsiebzig, neunundsiebzig, achtzig, einundachtzig, zweiundachtzig, dreiundachtzig, vierundachtzig, fünfundachtzig, sechsundachtzig, siebenundachtzig, achtundachtzig, neunundachtzig, neunzig, einundneunzig, zweiundneunzig, dreiundneunzig, vierundneunzig, fünfundneunzig, sechsundneunzig, siebenundneunzig, achtundneunzig, neunundneunzig, hundert.')
+# log(DEBUG_LEVEL_MIN, '')
+# log(DEBUG_LEVEL_MIN, 'Test Line Break:')
+# log(DEBUG_LEVEL_MIN, 'Direct next line')
+# log(DEBUG_LEVEL_MIN, 'Some line break in between \none sentence')
+# log(DEBUG_LEVEL_MIN, 'Next line')
+# exit(0)
+
+from linguflex_functions import linguflex_function, LinguFlexBase
 
 # Constants
 LINGU_FS_SLEEP_MAINCYCLE = 0.01 # don't raise too much or mic recording will fail
 
 # Dictionaries to store module instances
 all_modules = {}
-special_modules = {
-    InputProviderModule_IF: {},
-    SpeechRecognitionModule_IF: {},
-    TextToSpeechModule_IF: {},
-    LanguageProcessingModule_IF: {},
-    JsonActionProviderModule_IF: {},
+function_modules = {
+    LinguFlexBase : {}
 }
-
-
+special_modules = {
+    InputModule: {},
+    SpeechRecognitionModule: {},
+    TextToSpeechModule: {},
+    TextGeneratorModule: {},
+    ActionModule: {},
+}
 
 # Session logging setup
 if not os.path.exists("sessionlog"):
@@ -80,8 +78,8 @@ set_external_method(logtofile_and_inform_modules)
 
 
 
-log(DEBUG_LEVEL_MIN, "hello lingu")
-log(DEBUG_LEVEL_MAX, f"  logging debuglevel is {debug_level}")
+log(DEBUG_LEVEL_MIN, 'lingu hello')
+log(DEBUG_LEVEL_MAX, f'  logging debuglevel is {debug_level}')
 
 
 
@@ -93,28 +91,85 @@ def is_interface_name(name) -> bool:
             return True
     return False
 
-for module_path, _ in cfg.items("modules"):
-    log(DEBUG_LEVEL_MAX, f'  importing {module_path}')
-    module = importlib.import_module(module_path)
+# import json
+
+# openai_function_call_module = importlib.import_module("openai_function_call")
+
+modules = []
+for module_path, _ in items()('modules'):
+    log(DEBUG_LEVEL_MIN, f'importing {module_path}')
+
+    try:
+        module = importlib.import_module(module_path)
+        # log(DEBUG_LEVEL_MAX, f'  importing finished')
+        modules.append(module)
+    except Exception as e:
+        log(DEBUG_LEVEL_ERR, f'{module_path} {str(e)}')
+        raise(e)
+
+    # print (f'functions: {openai_function_call_module.functions}')
+    # print (f'classes: {openai_function_call_module.classes}')
+
     module_function = None
     for name, obj in inspect.getmembers(module):
-        for interface, container in special_modules.items():
+        for interface, container in function_modules.items():
             if not name == interface.__name__  and inspect.isclass(obj) and issubclass(obj, interface):
-                all_modules[module_path] = container[module_path] = obj_instance = obj()
+                log(DEBUG_LEVEL_MAX, f'  ├── · class {obj.__name__} imported')
+                #log(DEBUG_LEVEL_MAX, f'  imported function {obj.__name__}')
+                obj.register(obj, module_path)
+                container[module_path] = obj
                 module_function = interface.__name__
-                module_function = module_function.replace('Module_IF', '')
                 break
         else:
-            if not is_interface_name(name) and not name == "Module_IF" and inspect.isclass(obj) and issubclass(obj, Module_IF):
-                all_modules[module_path] = obj()
-    log_message = f'{module_path} ready'
-    if not module_function is None: log_message += f' ({module_function})'
-    log(DEBUG_LEVEL_MIN, log_message)
+            for interface, container in special_modules.items():
+                if not name == interface.__name__  and inspect.isclass(obj) and issubclass(obj, interface):
+                    log(DEBUG_LEVEL_MAX, f'  ├── {obj.__name__} ({interface.__name__}) imported')
+                    #log(DEBUG_LEVEL_MAX, f'  interface module object {obj.__name__} of {module_path} created for interface {interface.__name__}')
+
+                    all_modules[module_path] = container[module_path] = obj()
+                    module_function = interface.__name__
+                    module_function = module_function.replace('BaseModule', '')
+                    #all_modules[module_path].module_name = module_path
+                    break
+            else:
+                if not is_interface_name(name) and not name == "BaseModule" and inspect.isclass(obj) and issubclass(obj, BaseModule):
+                    log(DEBUG_LEVEL_MAX, f'  ├── {obj.__name__} imported')
+                    all_modules[module_path] = obj()
+    log_message = f'  {module_path} import complete'
+    #if not module_function is None: log_message += f' ({module_function})'
+    log(DEBUG_LEVEL_MAX, log_message)
+
+
+
+
+# # Convert to pretty printed JSON string
+# pretty_json = json.dumps(openai_function_call_module.openai_function_schemas, indent=4)
+
+# # Drucken Sie die Liste "openai_functions"
+# print(pretty_json)
+
+
+#  for module in modules:
+#      if hasattr(module, 'openai_function_schemas'):
+#          pretty_json = json.dumps(module.openai_function_schemas, indent=4)
+#          print(pretty_json)
+# # Convert to pretty printed JSON string
+
+
+# # Drucken Sie die Liste "openai_functions"
+# 
+
+
+
 
 
 # Launch server
-server = LinguFlexServer(all_modules, special_modules)
-log(DEBUG_LEVEL_MIN, "---Server up and ready---")
+server = LinguFlexServer(openai_function_call_module, all_modules, special_modules)
+log(DEBUG_LEVEL_MIN, "")
+log(DEBUG_LEVEL_MIN, "lingu ready")
+
+for module in modules:
+    module.__dict__['server'] = server
 
 for module_instance in all_modules.values():
     module_instance.server = server
@@ -122,29 +177,37 @@ for module_instance in all_modules.values():
 def main():
     try:
         is_running = True
-        message = LinguFlexMessage()
+        request = Request()
 
         while is_running:
             # Process modules' main cycle method
-            server.cycle(message)
+            server.cycle(request)
 
             # Handle input creation, processing, and output generation
-            message = server.process_message(message)
+            request = server.process_request(request)
 
             # Check for shutdown request
             if server.shutdown_request():
                 is_running = False
 
             # Sleep to reduce CPU usage when there's no input
-            if message.input is None:
+            if request.input is None:
                 time.sleep(LINGU_FS_SLEEP_MAINCYCLE)
 
+        log(DEBUG_LEVEL_MIN, 'Linguflex stopped')
+
         server.shutdown()            
+
+    except KeyboardInterrupt:
+
+        log(DEBUG_LEVEL_MIN, 'Linguflex stopped by user keyboard command')
+
+        server.shutdown()
 
     except Exception as e:
 
         # Catch all exceptions
-        print(f"An unexpected error occurred while running linguflex: {e}")
+        log(DEBUG_LEVEL_MIN, f'Linguflex stopped, unhandled exception occured: {str(e)}')
 
         # If you want to also print the traceback
         import traceback
@@ -152,9 +215,6 @@ def main():
 
         # Ensure server shutdown
         server.shutdown()            
-
-    except KeyboardInterrupt:
-        server.shutdown()
 
 if __name__ == "__main__":
     main()
