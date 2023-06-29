82042daa0f8e0056f3c2be4fab5b3d532ac093f6
Kolja Beigel <kolja.beigel@web.de>
2023-06-29 21:52:07 +0200

    Add files via upload

--- start_linguflex.bat
+++ start_linguflex.bat
@@ -9,36 +9,36 @@ cd /d %~dp0
 
 :: OpenAI API Key  https://platform.openai.com/
 :: - essential to run linguflex
-set LINGU_OPENAI_API_KEY=
+set OPENAI_API_KEY=
 
 :: Microsoft Azure API Key  https://portal.azure.com/
 :: - optional [azure_texttospeech]
-set LINGU_AZURE_SPEECH_KEY=
+set AZURE_SPEECH_KEY=
 
 :: Elevenlabs API Key  https://beta.elevenlabs.io/Elevenlabs
 :: - optional [elevenlabs_texttospeech]
-set LINGU_ELEVENLABS_SPEECH_KEY=
+set ELEVENLABS_SPEECH_KEY=
 
 ::  Google Cloud API key  https://console.cloud.google.com/
 :: - optional [media_playout] (API key needs access to the YouTube Data API v3)
 :: - optional [picture_search] (API key needs access to the Custom Search API)
-set LINGU_GOOGLE_API_KEY=
+set GOOGLE_API_KEY=
 
 ::  Search engine ID / Suchmaschinen-ID  https://cse.google.com/cse/all
 :: - optional [picture_search]
-set LINGU_GOOGLE_CX_KEY=
+set GOOGLE_CX_KEY=
 
 ::  Serp API key  https://serpapi.com/
 :: - optional [google_information]
-set LINGU_SERP_API_KEY=
+set SERP_API_KEY=
 
 ::   OpenWeatherMap API Key  https://openweathermap.org/api
 :: - optional [weather_forecast]
-set LINGU_OPENWEATHERMAP_API_KEY=
+set OPENWEATHERMAP_API_KEY=
 
-:: Check if LINGU_OPENAI_API_KEY is set
-if not defined LINGU_OPENAI_API_KEY (
-    echo LINGU_OPENAI_API_KEY environment variable is not set. 
+:: Check if OPENAI_API_KEY is set
+if not defined OPENAI_API_KEY (
+    echo OPENAI_API_KEY environment variable is not set. 
     echo OpenAI API Key expected in config.txt[openai_generator][api_key].
 )
 
