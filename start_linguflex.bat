9640634b823c24c175a6a09392aba3f19c39c918
Kolja Beigel <kolja.beigel@web.de>
2023-06-29 17:13:45 +0200

    Add files via upload

---
+++ start_linguflex.bat
@@ -0,0 +1,46 @@
+@echo off
+
+:: switch to current execution directory
+cd /d %~dp0
+
+
+:: setup environment
+::────────────────────────────────────
+
+:: OpenAI API Key  https://platform.openai.com/
+:: - essential to run linguflex
+set LINGU_OPENAI_API_KEY=
+
+:: Microsoft Azure API Key  https://portal.azure.com/
+:: - optional [azure_texttospeech]
+set LINGU_AZURE_SPEECH_KEY=
+
+:: Elevenlabs API Key  https://beta.elevenlabs.io/Elevenlabs
+:: - optional [elevenlabs_texttospeech]
+set LINGU_ELEVENLABS_SPEECH_KEY=
+
+::  Google Cloud API key  https://console.cloud.google.com/
+:: - optional [media_playout] (API key needs access to the YouTube Data API v3)
+:: - optional [picture_search] (API key needs access to the Custom Search API)
+set LINGU_GOOGLE_API_KEY=
+
+::  Search engine ID / Suchmaschinen-ID  https://cse.google.com/cse/all
+:: - optional [picture_search]
+set LINGU_GOOGLE_CX_KEY=
+
+::  Serp API key  https://serpapi.com/
+:: - optional [google_information]
+set LINGU_SERP_API_KEY=
+
+::   OpenWeatherMap API Key  https://openweathermap.org/api
+:: - optional [weather_forecast]
+set LINGU_OPENWEATHERMAP_API_KEY=
+
+
+:: start linguflex
+::────────────────────────────────────
+
+TITLE LinguFlex
+python linguflex.py
+pause
+cmd
