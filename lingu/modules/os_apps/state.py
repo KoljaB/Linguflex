b75418bd98eb30431c378dbd53a0e9e58d094104
KoljaB <kolja.beigel@web.de>
2024-10-21 23:56:21 +0200

    added os apps

--- lingu/modules/os_apps/state.py
+++ lingu/modules/os_apps/state.py
@@ -4,7 +4,7 @@ from lingu import State
 class OS_Apps_State(State):
     def __init__(self):
         super().__init__()
-        self.large_symbol = "🔧" # 🛠️🔨⚙️
+        self.large_symbol = "🔧" 
 
 
 state = OS_Apps_State()
